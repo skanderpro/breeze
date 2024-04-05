863e8f7f8f8600f5e493e0932cfa555a8c8ad498
nik19993 <mukoles@gmail.com>
2024-04-05 11:46:44 +0300

    add merchant relations

--- app/Models/Po.php
+++ app/Models/Po.php
@@ -9,4 +9,9 @@ class Po extends Model
     protected $fillable = [
         'u_id', 'companyId', 'selectMerchant', 'inputMerchant', 'poType', 'poPurpose', 'poMaterials', 'poProject', 'poProjectLocation', 'poValue', 'poCostSheet', 'poInvoice', 'poEMInvoice', 'poPod', 'poJobStatus', 'poFinanceStatus', 'poCompanyPo', 'poCancelled', 'poCancelledBy', 'poNotes', 'poCompleted', 'updated_at','alt_merchant_name',"alt_merchant_contact", "contract_id"
     ];
+
+
+    public function merchant(){
+        return $this->belongsTo('merchants', 'selectMerchant');
+    }
 }
