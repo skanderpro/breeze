fdb398f6214288df95eb257e25b17c050286fe1e
Bryniuk Valia <valyanychyporuk@gmail.com>
2024-05-17 10:23:47 +0300

    - update company lisr endpoit

--- app/Http/Controllers/Api/CompanyController.php
+++ app/Http/Controllers/Api/CompanyController.php
@@ -5,14 +5,19 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\CompanyResource;
 use App\Models\Company;
+use Illuminate\Http\Request;
 
 class CompanyController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $companies = Company::query()->whereNot('disabled', '1')->paginate();
+        $qb = Company::query();
+        $query = $request->input('query');
+        if (!empty($query)) {
+            $qb = $qb->where('companyName', 'like', "%{$query}%");
+        }
 
-        return CompanyResource::collection($companies);
+        return CompanyResource::collection($qb->paginate());
     }
 
     public function single(Company $company)
