9453f435d7a80bdfa4cf8d01fdab1d003aae1341
Bryniuk Valia <valyanychyporuk@gmail.com>
2024-06-07 04:47:01 +0300

    - update percentage

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -7,10 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class Company extends Model
 {
     protected $fillable = [
-        'companyName', 'companyPhone', 'companyFax', 'companyContact', 'companyContactEmail', 'companyAddress', 'disabled',
+        'companyName',
+        'companyPhone',
+        'companyFax',
+        'companyContact',
+        'companyContactEmail',
+        'companyAddress',
+        'disabled',
+        'agreed_rebate',
+        'agreed_markup',
     ];
-	
-	
+
+
     public function getRootCompanyAttribute(){
 		$company = Company::select('companies.*')->where('id', $this->parent_id)->first();
         return $company;
