3c617e827111c18a9c583b6904d9ba0ce04628f1
Bryniuk Valia <valyanychyporuk@gmail.com>
2024-06-14 14:07:51 +0300

    - update po request behaviour

--- app/Http/Controllers/Api/MerchantController.php
+++ app/Http/Controllers/Api/MerchantController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\Traits\MerchantControllerTrait;
 use App\Http\Resources\MerchantResource;
 use App\Models\Merchant;
 use Illuminate\Http\Request;
 
 class MerchantController extends Controller
 {
+    use MerchantControllerTrait;
+
     public function index(Request $request)
     {
         $qb = Merchant::query();
@@ -105,5 +108,27 @@ class MerchantController extends Controller
         }
 
         return MerchantResource::collection($qb->get());
+    }
+
+    public function createMerchant(Request $request)
+    {
+        $merchant = $this->store($request);
+
+        return MerchantResource::make($merchant);
+    }
+
+    public function updateMerchant($id, Request $request)
+    {
+        $merchant = $this->update($id, $request);
+
+        return MerchantResource::make($merchant);
+    }
+
+    public function toggle(Merchant $merchant, Request $request)
+    {
+        $merchant->disabled = !$merchant->disabled;
+        $merchant->save();
+
+        return MerchantResource::make($merchant);
     }
 }
