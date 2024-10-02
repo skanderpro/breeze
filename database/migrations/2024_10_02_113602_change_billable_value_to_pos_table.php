3f4500e01eb2be3eb2b6a59fa725ce1d2cff2e26
Skander <ol.ryha@gmail.com>
2024-10-02 14:45:25 +0300

    change bill value to null

---
+++ database/migrations/2024_10_02_113602_change_billable_value_to_pos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::table("pos", function (Blueprint $table) {
+      $table
+        ->float("billable_value_final")
+        ->nullable()
+        ->change();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::table("pos", function (Blueprint $table) {
+      $table
+        ->float("billable_value_final")
+        ->default(0)
+        ->change();
+    });
+  }
+};
