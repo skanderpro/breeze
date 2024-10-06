af661181307c1633bdcce25b50ac8d71aa785136
Ваше Имя <you@example.com>
2024-10-06 03:56:57 +0300

    - update branches

--- app/Http/Resources/MerchantResource.php
+++ app/Http/Resources/MerchantResource.php
@@ -14,12 +14,15 @@ class MerchantResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+
+
         return [
             'id' => $this->resource->id,
             'name' => $this->resource->merchantName,
             'merchant_id' => $this->resource->merchantId,
             'address_1' => $this->resource->merchantAddress1,
             'address_2' => $this->resource->merchantAddress2,
+            'address' => $this->resource->getAddress(),
             'country' => $this->resource->merchantCountry,
             'post_code' => $this->resource->merchantPostcode,
             'phone' => $this->resource->merchantPhone,
